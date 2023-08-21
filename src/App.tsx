eecb5a3e143943f58f9a6f467148fd140b16d105
wulin <admin@MacBook.local>
2023-08-21 15:15:42 +0800

    feat: 修改主题样式；

--- src/App.tsx
+++ src/App.tsx
@@ -1,4 +1,9 @@
-import { darkTheme, GlobalThemeOverrides } from 'naive-ui'
+import {
+  darkTheme,
+  GlobalThemeOverrides,
+  NConfigProvider,
+  NMessageProvider
+} from 'naive-ui'
 
 import { useThemeStore } from '@/store/theme'
 import themeList from '@/themes'
@@ -11,22 +16,27 @@ export default defineComponent({
       themeStore.darkTheme ? darkTheme : undefined
     )
 
+    return {
+      currentTheme
+    }
+  },
+  render() {
     const themeOverrides: GlobalThemeOverrides =
-      themeList[currentTheme ? 'dark' : 'light']
+      themeList[this.currentTheme ? 'dark' : 'light']
 
-    return () => (
-      <n-configProvider
-        theme={currentTheme.value}
-        theme-overrides={themeOverrides}
+    return (
+      <NConfigProvider
+        theme={this.currentTheme}
+        themeOverrides={themeOverrides}
         style={{
           width: '100%',
           height: '100vh'
         }}
       >
-        <n-messageProvider>
+        <NMessageProvider>
           <router-view />
-        </n-messageProvider>
-      </n-configProvider>
+        </NMessageProvider>
+      </NConfigProvider>
     )
   }
 })
