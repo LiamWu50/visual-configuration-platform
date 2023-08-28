0f57b534f5f37750c2d5cd189cc4ee78642b72df
wulin <admin@MacBook.local>
2023-08-28 15:53:55 +0800

    build: 添加vite的ssetsInclude配置项；

--- vite.config.ts
+++ vite.config.ts
@@ -17,6 +17,7 @@ export default defineConfig(({ command, mode }) => {
         '@': path.resolve(__dirname, 'src')
       }
     },
+    assetsInclude: ['**/*.gltf'],
     server: {
       host: '0.0.0.0', // 默认为'127.0.0.1'，如果将此设置为 `0.0.0.0` 或者 `true` 将监听所有地址，包括局域网和公网地址
       port: env.VITE_PORT, // 端口
