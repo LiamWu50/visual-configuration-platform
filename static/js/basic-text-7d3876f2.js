93b456694d956755606ea254299d0cf7e1563c66
LiamWu50 <34302019+LiamWu50@users.noreply.github.com>
2023-08-28 02:25:57 +0000

    Deploying to gh-pages from @ LiamWu50/visual-configuration-platform@2dd87babb32184fec1e78a97e4c989330df8386c 🚀

---
+++ static/js/basic-text-7d3876f2.js
@@ -0,0 +1,2 @@
+import{d as e,B as a,D as s}from"./index-162bca0c.js";const t=e({name:"BasicText",props:{dataSource:{type:Object,default:()=>({})}},setup(e){},render:()=>a("span",null,[s("普通文本")])});export{t as default};
+//# sourceMappingURL=basic-text-7d3876f2.js.map
--- static/js/index-8312a053.js
+++
@@ -1,2 +0,0 @@
-import{d as e,B as s,D as a}from"./index-e351b794.js";const r=e({name:"Project",setup(){},render:()=>s("div",null,[a("项目列表")])});export{r as default};
-//# sourceMappingURL=index-8312a053.js.map
