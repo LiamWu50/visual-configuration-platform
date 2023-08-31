59f55bd4363c85e97a8b077fa8610b452b3e7686
wulin <admin@MacBook.local>
2023-08-31 11:41:17 +0800

    feat: 完成地图和三维场景模块列表；

--- src/views/projects/draft/components/map-list.tsx
+++ src/views/projects/draft/components/map-list.tsx
@@ -1,7 +1,39 @@
+import banner from '@/assets/images/projects/gis-banner.png'
+
+import Filters from './filters'
+import styles from './list.module.scss'
+
 const MapList = defineComponent({
   name: 'map-list',
+  setup() {
+    const list = ref([0, 1, 2, 3, 4, 5, 6, 7, 8, 9])
+
+    return {
+      list
+    }
+  },
   render() {
-    return <div>map-list</div>
+    const renderItem = () =>
+      this.list.map(() => (
+        <div class={styles.item}>
+          <div class={styles.banner}>
+            <img src={banner} />
+          </div>
+          <div class={styles.info}>
+            <div class={styles.name}>三维GIS场景</div>
+            <div class={styles['last-time']}>更新于1月26日 15:37</div>
+          </div>
+        </div>
+      ))
+
+    return (
+      <div class={styles.container}>
+        <div class={styles.sort}>
+          <Filters />
+        </div>
+        <div class={styles.list}>{renderItem()}</div>
+      </div>
+    )
   }
 })
 export default MapList
--- src/views/projects/draft/components/three-list.tsx
+++ src/views/projects/draft/components/three-list.tsx
@@ -1,7 +1,39 @@
+import banner from '@/assets/images/projects/three-banner.png'
+
+import Filters from './filters'
+import styles from './list.module.scss'
+
 const ThreeList = defineComponent({
   name: 'three-list',
+  setup() {
+    const list = ref([0, 1, 2, 3, 4, 5, 6, 7, 8, 9])
+
+    return {
+      list
+    }
+  },
   render() {
-    return <div>three-list</div>
+    const renderItem = () =>
+      this.list.map(() => (
+        <div class={styles.item}>
+          <div class={styles.banner}>
+            <img src={banner} />
+          </div>
+          <div class={styles.info}>
+            <div class={styles.name}>ThreeJs场景</div>
+            <div class={styles['last-time']}>更新于9月18日 4:52</div>
+          </div>
+        </div>
+      ))
+
+    return (
+      <div class={styles.container}>
+        <div class={styles.sort}>
+          <Filters />
+        </div>
+        <div class={styles.list}>{renderItem()}</div>
+      </div>
+    )
   }
 })
 export default ThreeList
