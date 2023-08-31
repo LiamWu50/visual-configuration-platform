6795e0c7734add018f92482a9baa3aec71365d29
wulin <admin@MacBook.local>
2023-08-31 18:18:34 +0800

    feat: 开发画布样式设置组件；

---
+++ src/views/design-space/chart/configuration/canvas-setting.tsx
@@ -0,0 +1,85 @@
+import { CaretForwardOutline } from '@vicons/ionicons5'
+import { debounce } from 'lodash'
+import {
+  NCollapse,
+  NCollapseItem,
+  NForm,
+  NFormItem,
+  NGi,
+  NGrid,
+  NIcon,
+  NInputNumber
+} from 'naive-ui'
+
+import { CanvasStyle } from '@/primitives/types'
+import { useEditorStore } from '@/store/editor/index'
+
+export default defineComponent({
+  name: 'canvasSettings',
+  setup() {
+    const editorStore = useEditorStore()
+    const { canvasStyle } = storeToRefs(editorStore)
+    const curStyleAttrs = computed(() => canvasStyle.value as CanvasStyle)
+
+    const canvasAttrsSource = [
+      {
+        label: 'W',
+        type: 'width'
+      },
+      {
+        label: 'H',
+        type: 'height'
+      }
+    ]
+
+    watch(curStyleAttrs, (v) =>
+      debounce(() => editorStore.setCanvasStyle(v), 300)
+    )
+
+    return () => (
+      <NCollapse
+        defaultExpandedNames='baseAttrs'
+        v-slots={{
+          arrow: () => (
+            <NIcon size={12}>
+              <CaretForwardOutline />
+            </NIcon>
+          )
+        }}
+      >
+        <NCollapseItem
+          title='画布样式'
+          name='baseAttrs'
+          v-slots={{
+            default: () => (
+              <NForm size='small' showLabel={false}>
+                <NGrid xGap={12} cols={2}>
+                  {canvasAttrsSource.map((item) => (
+                    <NGi>
+                      <NFormItem>
+                        <NInputNumber
+                          v-model:value={
+                            curStyleAttrs.value![item.type as keyof CanvasStyle]
+                          }
+                          max={9999}
+                          min={-9999}
+                          showButton={false}
+                          precision={0}
+                          clearable={true}
+                          placeholder={`请输入${item.label}`}
+                          v-slots={{
+                            prefix: () => item.label
+                          }}
+                        />
+                      </NFormItem>
+                    </NGi>
+                  ))}
+                </NGrid>
+              </NForm>
+            )
+          }}
+        ></NCollapseItem>
+      </NCollapse>
+    )
+  }
+})
--- src/views/design-space/chart/configuration/style-settings.tsx
+++ src/views/design-space/chart/configuration/style-settings.tsx
@@ -1,18 +1,21 @@
 import { useEditorStore } from '@/store/editor/index'
 
+import CanvasSettings from './canvas-setting'
 import styles from './index.module.scss'
 
 export default defineComponent({
-  name: 'StyleSettings',
+  name: 'styleSettings',
   setup() {
     const editorStore = useEditorStore()
     const { curPrimitive } = storeToRefs(editorStore)
 
     return () => (
       <div class={styles.styleSettings}>
-        {curPrimitive.value
-          ? h(resolveComponent(`${curPrimitive.value?.cName}Attrs`))
-          : null}
+        {curPrimitive.value ? (
+          h(resolveComponent(`${curPrimitive.value?.cName}Attrs`))
+        ) : (
+          <CanvasSettings />
+        )}
       </div>
     )
   }
