451fe262908cd5eb400df4802367de73f7aa3dd2
wulin <wulin@hualifangyuan.com>
2023-08-31 22:43:52 +0800

    feat: 添加三种类型预览模块；

--- src/views/preview/index.tsx
+++ src/views/preview/index.tsx
@@ -1,16 +1,26 @@
-import { usePreviewStore } from '@/store/preview'
+import { useRoute } from 'vue-router'
+
+import Chart from './chart'
+import Map from './map'
+import Three from './three'
 
 const Preview = defineComponent({
   name: 'preview',
   setup() {
-    const previewStore = usePreviewStore()
-    const { chartForPreview } = storeToRefs(previewStore)
-    console.log('chartForPreview', chartForPreview)
+    const route = useRoute()
+    const stage = route.query.stage
 
-    return {}
+    return {
+      stage
+    }
   },
   render() {
-    return <div>preview</div>
+    const stage = this.stage
+    return (
+      <>
+        {stage === 'chart' ? <Chart /> : stage === 'map' ? <Map /> : <Three />}
+      </>
+    )
   }
 })
 export default Preview
